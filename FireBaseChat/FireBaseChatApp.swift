11c27b486b12b059938fe095cda4455bf5eb547f
Frank Su <candyline25@gmail.com>
2021-12-31 15:31:26 -0800

    Initial Commit

---
+++ FireBaseChat/FireBaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseChatApp.swift
+//  FireBaseChat
+//
+//  Created by candyline on 2021-12-31.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ FireBaseChat/LoginView.swift
@@ -0,0 +1,80 @@
+//
+//  LoginView.swift
+//  FireBaseChat
+//
+//  Created by candyline on 2021-12-31.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 16) {
+                    Picker("Create Account", selection: $isLoginMode) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    if !isLoginMode {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.never)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(Color.white)
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Login" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }
+                        .background(Color.blue)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05)))
+        }
+        
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("Should login with existing Firebase auth stuff")
+        } else {
+            print("Create a new account and figure out how to store the image in Firebase")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
